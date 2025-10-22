00788984749faf2cd53b6f8e9c6de8b753729c22
unknown <shayshn1234@gmail.com>
2025-10-22 12:12:16 +0300

    exercise

---
+++ w1.py
@@ -0,0 +1,75 @@
+# #1
+# def start_f(func):
+#     def s_f():
+#         print("start Function") 
+#         func()
+#         print("End Function")
+#     return s_f
+
+# @start_f
+# def name():
+#    print("midlle")
+   
+# name() 
+
+# #2
+# import time
+
+# def timing_run(func):
+#     def times():
+#         start = time.time()
+#         func()
+#         end = time.time()
+#         print(end - start )
+#     return times
+
+# @timing_run
+# def runs():
+#     for i in range(1000000):
+#         print(i,end=" ")
+
+# runs()
+    
+# #3
+# def logging_arguments(func):
+#     def print_arguments(*args):
+#         print(*args)
+#         func(*args)
+#         print("end!")
+#     return print_arguments
+
+# @logging_arguments
+# def starting():
+#     print(["shay","shay","nechmad","nechmad"])
+
+# starting()
+        
+# #4
+# def uppercase(func):
+#     def make_upper(name):
+#        return func(name).upper()
+#     return make_upper
+
+# @uppercase
+# def begain(name):
+#     return name
+    
+# print(begain("shay"))    
+
+#5
+def count_calls(func):
+    counter = 0
+    def wraper():
+        nonlocal counter
+        func()
+        counter +=1
+        print(counter)
+    return wraper
+
+@count_calls
+def starting():
+    print("starting counter!!!")
+    
+starting()
+starting()
+starting() 
